9a1601039129f56b2caba7d5c08c6bd9d5076ec1
Yingzhou <zyzstat@foxmail.com>
2021-10-18 09:38:20 +0800

    Add news selection for 7 categories

--- cnbeta.py
+++ cnbeta.py
@@ -13,32 +13,66 @@ def handle_request(url):
 
 
 def get_text(href):
-    request = handle_request(href)
-    content = urllib.request.urlopen(request).read().decode("utf8")
-    soup = BeautifulSoup(content, "lxml")
-    odiv = soup.find_all("div", class_="item")
-    with open("./cnbeta_txt.txt", "w") as writer:
-        writer.write("------------------热门-----------------\n")
-        strip_mark = 0
-        for cont in odiv:
-            if "详细内容" in cont.text:
-                strip_mark = 1
-            text = cont.text.replace("\n\n\n", "\n")
-            if "详细内容" in text:
-                writer.write("\n\n\n\n------------------简讯------------------\n")
-            try:
-                text = text[: text.index("详细内容")]
-            except:
-                pass
-            if strip_mark == 1:
-                if text.strip("\n").strip() != '':
-                    writer.write(text.strip("\n").strip())
-                    writer.write("\n----------------------------------------")
+    category = input(
+        """
+Enter a category:
+                    movie -- 影视
+                    music -- 音乐
+                    game -- 游戏
+                    comic -- 动漫
+                    funny -- 趣闻
+                    science -- 科学
+                    soft -- 软件\n
+>>"""
+    )
+    try:
+        if category != "":
+            href = href + "/category/{}.htm".format(category)
+        request = handle_request(href)
+        content = urllib.request.urlopen(request).read().decode("utf8")
+        soup = BeautifulSoup(content, "lxml")
+        odiv = soup.find_all("div", class_="item")
+        with open("./cnbeta_txt.txt", "w") as writer:
+            if category == "":
+                writer.write("------------------热门-----------------\n")
+                strip_mark = 0
+                for cont in odiv:
+                    if "详细内容" in cont.text:
+                        strip_mark = 1
+                    text = cont.text.replace("\n\n\n", "\n")
+                    if "详细内容" in text:
+                        writer.write("\n\n\n\n------------------简讯------------------\n")
+                    try:
+                        text = text[: text.index("详细内容")]
+                    except:
+                        pass
+                    if strip_mark == 1:
+                        if text.strip("\n").strip() != "":
+                            writer.write(text.strip("\n").strip())
+                            writer.write("\n----------------------------------------")
+                    else:
+                        writer.write(text)
             else:
-                writer.write(text)
+                writer.write("------------------{}-----------------".format(category))
+                strip_mark = 0
+                for cont in odiv:
+                    if "详细内容" in cont.text:
+                        strip_mark = 1
+                    text = cont.text.replace("\n\n\n", "\n")
+                    if "详细内容" in text:
+                        writer.write("\n\n\n\n------------------简讯------------------\n")
+                    try:
+                        text = text[: text.index("详细内容")]
+                    except:
+                        pass
+                    if strip_mark == 1:
+                        if text.strip("\n").strip() != "":
+                            writer.write(text.strip("\n").strip())
+                            writer.write("\n----------------------------------------")
+    except:
+        "Input error, try again"
 
 
 get_text("http://cnbeta.com")
 
-print("All recent cnbeta data get, :)!!!")
-
+print("\nEnjoy it\n:)")
